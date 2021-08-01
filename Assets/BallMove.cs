77141d5abe20ec1411d74b64f9a2400421ea6485
WDEXcourse <wdkawasaki.online@gmail.com>
2021-08-01 18:16:32 +0900

    テキストが終わった

--- Assets/BallMove.cs
+++ Assets/BallMove.cs
@@ -6,18 +6,54 @@ public class BallMove : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public Vector3 Startposition;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.AddForce(10, 0, 0, ForceMode.Impulse);
-
+        rb.AddForce(45, 0, 0, ForceMode.Impulse);
+		Startposition = transform.position;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -20)
+        {
+            transform.position = Startposition;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
