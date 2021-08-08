bc010825709f193f16c26dc7722e951961adb17b
WDEXcourse <wdkawasaki.online@gmail.com>
2021-08-08 18:01:21 +0900

    ゴールを作った
    ブロックで分けて穴を開けるで座標をすぎたら勝敗を表示する。

---
+++ Assets/GameClear.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClear : MonoBehaviour
+{
+    public GameObject ClearText;
+    public GameObject Ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClearText.SetActive(false);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x)
+
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
